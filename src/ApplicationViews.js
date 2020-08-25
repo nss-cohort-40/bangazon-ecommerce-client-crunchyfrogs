d483dc93c96dd722eb033d58c3ec7df476bfe5ac
John Bain <johnknoxbain@gmail.com>
2020-08-25 09:25:45 -0500

    starting up react

---
+++ src/ApplicationViews.js
@@ -0,0 +1,16 @@
+import { Route, Redirect } from 'react-router-dom';
+import React from 'react';
+
+
+const ApplicationViews = props => {
+    <>
+      <Route
+        exact
+        path="/"
+        render= {props => null}
+      />
+    </>
+}
+
+
+export default ApplicationViews
