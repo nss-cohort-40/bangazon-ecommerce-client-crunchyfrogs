8112bf39bca97456584c5ce4f45b42f2fef94c05
John Bain <johnknoxbain@gmail.com>
2020-08-26 13:55:55 -0500

    skeleton

--- src/components/productdetails/ProductDetails.js
+++ src/components/productdetails/ProductDetails.js
@@ -3,6 +3,20 @@ import React, { useRef, useState } from "react"
 
 const ProductDetails = props => {
 
+    const [product, setProduct] = useState({})
+
+    const getProduct = () => {
+        fetch(`http://localhost:8000/product/${props.productId}`, {
+            "method": "GET",
+            "headers": {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "Authorization": `Token ${localStorage.getItem("bangazon_token")}`
+            }
+        })
+        .then(response => response.json())
+        .then(products => setProduct(products))
+    }
 
     return (
         <div>
