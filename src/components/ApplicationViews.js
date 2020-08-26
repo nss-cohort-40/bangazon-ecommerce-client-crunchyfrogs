162ed85e8c38a4d4ba1e228dd1139f01b3e26ef5
John Bain <johnknoxbain@gmail.com>
2020-08-26 13:46:32 -0500

    setup for product details done

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router } from "react-router-dom"
 import React from 'react';
 import Login from "./auth/Login"
 import Register from "./auth/Register"
+import ProductDetails from './productdetails/ProductDetails';
 
 
 const ApplicationViews = () => {
@@ -15,6 +16,11 @@ const ApplicationViews = () => {
         <Route
             exact path="/login" render={props => {
                 return <Login {...props}/>
+            }}
+        />
+        <Route
+            exact path="/products/:productId(\d+)" render={props => {
+                return <ProductDetails productId={parseInt(props.match.params.productId)} {...props}/>
             }}
         />
     </>)
---
+++ src/components/productdetails/ProductDetails.js
@@ -0,0 +1,15 @@
+import React, { useRef, useState } from "react"
+
+
+const ProductDetails = props => {
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+
+export default ProductDetails
