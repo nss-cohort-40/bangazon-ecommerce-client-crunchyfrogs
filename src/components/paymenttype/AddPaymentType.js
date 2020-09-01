e9ed16c4eaca95eebb358d92ecf2ff9efd5a7fdc
Eli Lavoie <storey14@googlemail.comgit config --global user.name Eli>
2020-09-01 14:19:44 -0500

    added check to block customers from adding expired payment, as well as tweaked UI for adding payment type to be more user friendly

--- src/components/paymenttype/AddPaymentType.js
+++ src/components/paymenttype/AddPaymentType.js
@@ -1,22 +1,31 @@
 import React, { useRef, useState } from "react"
+import "./AddPaymentType.css"
 
 const AddPayment = props => {
     const merchantName = useRef()
     const accountNumber = useRef()
-    const expirationDate = useRef()
+    const expirationMonth = useRef()
+    const expirationYear = useRef()
     const [ toggleModal, setToggleModal ] = useState(false)
     let [ paymentButton, setPaymentButton ] = useState("Add a payment type")
 
-
     const handleAddPayment = (evt) => {
         evt.preventDefault()
+        
+        const expirationDate = expirationYear.current.value + "-" + expirationMonth.current.value + "-01" 
+        const date_today = Date.now()
+        const exp_date = Date.parse(expirationDate)
 
+        if(date_today >= exp_date){
+            window.alert("The card you are trying to add has already expired. Please use a valid payment method.")
+            return
+        }
+        else{
         const paymentType = {
             merchant_name: merchantName.current.value,
             account_number: accountNumber.current.value,
-            expiration_date: expirationDate.current.value
+            expiration_date: expirationDate
         }
-
 
         fetch(`http://localhost:8000/paymenttype`, {
             "method": "POST",
@@ -29,6 +38,7 @@ const AddPayment = props => {
         })
         .then(props.getPayments())
     }
+}
 
     const handleModal = () => {
         if (toggleModal) {
@@ -46,13 +56,41 @@ const AddPayment = props => {
         <>
             <button onClick={handleModal}>{paymentButton}</button>
             <dialog id="add-payment-type-dialog">
-                <form onSubmit={handleAddPayment}>
-                    <label for="merchant_name">Merchant</label>
+                <form id="add-payment-form" onSubmit={handleAddPayment}>
+                    <label for="merchant_name">Merchant: </label>
                     <input id="merchant_name" ref={merchantName}></input>
-                    <label for="account_number">Account Number</label>
+                    <label for="account_number">Account Number: </label>
                     <input id="account_number" ref={accountNumber}></input>
-                    <label for="expiration_date">Expiration Date</label>
-                    <input id="expiration_date" type="date" ref={expirationDate}></input>
+                    <div id="exp-date-selector">
+                        <label for="expiration_month">Expiration Date: </label>
+                        <select id="expiration_month" ref={expirationMonth}>
+                            <option value="1">01</option>
+                            <option value="2">02</option>
+                            <option value="3">03</option>
+                            <option value="4">04</option>
+                            <option value="5">05</option>
+                            <option value="6">06</option>
+                            <option value="7">07</option>
+                            <option value="8">08</option>
+                            <option value="9">09</option>
+                            <option value="10">10</option>
+                            <option value="11">11</option>
+                            <option value="12">12</option>
+                        </select>
+                        <label for="expiration_year">/</label>
+                        <select id="expiration_year" ref={expirationYear}>
+                            <option value="2020">20</option>
+                            <option value="2021">21</option>
+                            <option value="2022">22</option>
+                            <option value="2023">23</option>
+                            <option value="2024">24</option>
+                            <option value="2025">25</option>
+                            <option value="2026">26</option>
+                            <option value="2027">27</option>
+                            <option value="2028">28</option>
+                            <option value="2029">29</option>
+                        </select>
+                    </div>
                     <button type="submit" onClick={handleModal}>Add Payment Type</button>
                 </form>
             </dialog>
