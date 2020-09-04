2bbb1c0eff22f1ae11c648e3435701e980683093
felipetempus <felipemouradev@gmail.com>
2020-09-04 15:54:46 -0400

    Merged with the master

--- src/components/product/ProductCard.js
+++ src/components/product/ProductCard.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Api from '../../api/module'
+import { Link } from 'react-router-dom'
 
 import { DeleteButton, DeleteFromOrderButton } from "./Buttons"
 
 
 const ProductCard = (props) => {
-
+    let token = localStorage.getItem("bangazon_token")
     let productId = props.product.id
 
 
@@ -20,13 +21,13 @@ const ProductCard = (props) => {
                 <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Question-mark.jpg/900px-Question-mark.jpg"} className="card-img-top" style={{ width: '75px', height: '75px' }} alt={props.product.title} />
             }
             <div className="card-body">
-                <h5 className="card-title">{props.product.title}</h5>
+                {token ? <Link to={`/products/${productId}`}><h5 className="card-title">{props.product.title}</h5></Link> : <h5 className="card-title">{props.product.title}</h5>}
                 <p className="card-text">{props.product.description}</p>
             </div>
             <ul className="list-group list-group-flush">
                 <li className="list-group-item">Price: ${props.product.price}</li>
                 <li className="list-group-item">{props.product.quantity} unit(s) available </li>
-                <li className="list-group-item">{props.product.location}{props.product.local_delivery ? " -- Local delivery available" : null}</li>
+                <li className="list-group-item">Location: {props.product.location}{props.product.local_delivery ? " -- Local delivery available" : null}</li>
             </ul>
             <div className="card-body">
                 {props.customer ?
